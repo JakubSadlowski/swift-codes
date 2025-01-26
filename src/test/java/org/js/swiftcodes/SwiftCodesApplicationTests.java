334adc31fe98cde69d0b82ebeaae765ce46e0fbb
JakubSadlowski <76012344+JakubSadlowski@users.noreply.github.com>
2025-01-26 15:52:18 +0100

    Initial service setup.

---
+++ src/test/java/org/js/swiftcodes/SwiftCodesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.js.swiftcodes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftCodesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
