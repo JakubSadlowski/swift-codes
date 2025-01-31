b82e9072d15525dab00847440268ca28d499871c
JakubSadlowski <76012344+JakubSadlowski@users.noreply.github.com>
2025-01-31 19:23:54 +0100

    Changed SwiftCodesReader and added logging.

---
+++ src/main/java/org/js/swiftcodes/service/exceptions/GeneralException.java
@@ -0,0 +1,7 @@
+package org.js.swiftcodes.service.exceptions;
+
+public class GeneralException extends RuntimeException {
+    public GeneralException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
--- src/main/java/org/js/swiftcodes/service/reader/SwiftCodesReader.java
+++ src/main/java/org/js/swiftcodes/service/reader/SwiftCodesReader.java
@@ -1,33 +1,43 @@
 package org.js.swiftcodes.service.reader;
 
+import lombok.extern.apachecommons.CommonsLog;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.js.swiftcodes.service.exceptions.GeneralException;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+@CommonsLog
 public class SwiftCodesReader {
-    public static void readFile(String fileName) throws IOException {
-        FileInputStream file = new FileInputStream(new File(fileName));
-        Workbook workbook = new XSSFWorkbook(file);
-        Sheet sheet = workbook.getSheetAt(0);
+    private SwiftCodesReader() {
+    }
 
-        for (Row row : sheet) {
-            for (Cell cell : row) {
-                switch (cell.getCellType()) {
-                    case STRING -> System.out.print(cell.getStringCellValue() + "\t");
-                    case NUMERIC -> System.out.print(cell.getNumericCellValue() + "\t");
-                    default -> System.out.print("?");
+    public static void readSwiftCodesFile(String fileName) {
+        try (FileInputStream file = new FileInputStream(new File(fileName))) {
+            try (Workbook workbook = new XSSFWorkbook(file)) {
+                Sheet sheet = workbook.getSheetAt(0);
+
+                for (Row row : sheet) {
+                    for (Cell cell : row) {
+                        switch (cell.getCellType()) {
+                            case STRING -> System.out.print(cell.getStringCellValue() + "\t");
+                            case NUMERIC -> System.out.print(cell.getNumericCellValue() + "\t");
+                            default -> System.out.print("?");
+                        }
+                    }
+                    System.out.println();
                 }
             }
-            System.out.println();
+
+        } catch (IOException e) {
+            log.error(String.format("Failed to read file %s", fileName), e);
+            throw new GeneralException(String.format("Failed to read file %s", fileName), e);
         }
-        
-        workbook.close();
-        file.close();
+
     }
 }
