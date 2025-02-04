ee4d234a22d185612cdc269630d36a7dd2d53f80
JakubSadlowski <76012344+JakubSadlowski@users.noreply.github.com>
2025-02-04 23:56:23 +0100

    1) Added first rest controller. 2) Changed pom structure and deleted unused lines of code. 3) Added open api yaml file and exposed rest api.

--- src/main/java/org/js/swiftcodes/config/SwaggerOpenAPIConfig.java
+++ src/main/java/org/js/swiftcodes/config/SwaggerOpenAPIConfig.java
@@ -1,4 +1,38 @@
 package org.js.swiftcodes.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.util.Properties;
+
+@OpenAPIDefinition(servers = @Server(url = "/", description = "https://localhost:8080"))
+@Configuration
+@RequiredArgsConstructor
 public class SwaggerOpenAPIConfig {
+    @Bean
+    public BuildProperties createProperties() {
+        return new org.springframework.boot.info.BuildProperties(new Properties());
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() throws IOException {
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        return mapper.readValue(new ClassPathResource("swift-codes-open-api.yaml").getInputStream(), OpenAPI.class);
+    }
+
+    private Info apiInfo() {
+        return new Info().title("SWIFT Codes")
+            .description("API for Animals Shelter management.")
+            .version(createProperties().getVersion());
+    }
 }
