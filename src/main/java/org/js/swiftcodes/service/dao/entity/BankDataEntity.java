ebfc23016dc8596ef9b8fe0624ce0e8536362134
JakubSadlowski <76012344+JakubSadlowski@users.noreply.github.com>
2025-02-05 19:21:48 +0100

    1) Added BankDataMapper to map read bank data to BankDataEntity. 2) Added Insert to SwiftCodesMapper to insert bank data from list. Insert is working properly with real database but not properly handled adding where there already exists given id.

--- src/main/java/org/js/swiftcodes/service/dao/entity/BankDataEntity.java
+++ src/main/java/org/js/swiftcodes/service/dao/entity/BankDataEntity.java
@@ -1,10 +1,12 @@
 package org.js.swiftcodes.service.dao.entity;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class BankDataEntity {
-    private final int id;
+    private final Integer id;
     private final String swiftCode;
     private final String countryIso2Code;
     private final boolean isHeadquarter;
--- src/test/java/org/js/swiftcodes/service/dao/mapper/SwiftCodesMapperIT.java
+++ src/test/java/org/js/swiftcodes/service/dao/mapper/SwiftCodesMapperIT.java
@@ -1,7 +1,11 @@
 package org.js.swiftcodes.service.dao.mapper;
 
+import org.js.swiftcodes.api.mappers.BankDataMapper;
+import org.js.swiftcodes.api.model.BankData;
 import org.js.swiftcodes.config.DatabaseConfig;
 import org.js.swiftcodes.service.dao.entity.BankDataEntity;
+import org.js.swiftcodes.service.model.SwiftCode;
+import org.js.swiftcodes.service.reader.SwiftCodesFileReader;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
 import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 @ContextConfiguration(classes = { DatabaseConfig.class })
@@ -26,6 +31,22 @@ class SwiftCodesMapperIT {
         // then
         Assertions.assertNotNull(swiftCodesEntities);
         Assertions.assertEquals(1, swiftCodesEntities.size());
+    }
+
+    @Test
+    void insertSwiftCodesListIntoDatabase() {
+        // Given
+        Map<SwiftCode, BankData> swiftCodes = SwiftCodesFileReader.newInstance()
+            .readSwiftCodesFile("src/test/resources/test_swift_codes.xlsx");
+
+        List<BankDataEntity> bankDataEntities = BankDataMapper.mapToBankDataEntitiesListWithIds(swiftCodes.values().stream().toList());
+
+        // When
+        swiftCodesMapper.insert(bankDataEntities);
+        List<BankDataEntity> insertedSwiftCodesList = swiftCodesMapper.fetchAllSwiftCodes();
+
+        // Then
+        Assertions.assertNotNull(insertedSwiftCodesList);
     }
 
 }
