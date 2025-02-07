74582d6d33a18e3136fae3754290f24f4053c6fc
JakubSadlowski <76012344+JakubSadlowski@users.noreply.github.com>
2025-02-07 12:16:43 +0100

    Had to go back to 3.2.3 version of spring because the newest version is unstable and not compatible with the 2.1.0 version of springdoc.

--- src/test/java/org/js/swiftcodes/api/SwiftCodeControllerTestIT.java
+++ src/test/java/org/js/swiftcodes/api/SwiftCodeControllerTestIT.java
@@ -11,10 +11,10 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -22,7 +22,7 @@ import static org.mockito.Mockito.verify;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ExtendWith(MockitoExtension.class)
 class SwiftCodeControllerTestIT {
-    @MockitoBean
+    @MockBean
     private BankDataMapper bankDataMapper;
 
     @Autowired
