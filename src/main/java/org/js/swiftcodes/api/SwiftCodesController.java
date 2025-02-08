ac1e87fcfc6257b97d8c755dc082775944da736b
JakubSadlowski <76012344+JakubSadlowski@users.noreply.github.com>
2025-02-08 23:33:14 +0100

    Delete endpoint working now need to test this.

--- src/main/java/org/js/swiftcodes/api/SwiftCodesController.java
+++ src/main/java/org/js/swiftcodes/api/SwiftCodesController.java
@@ -6,6 +6,7 @@ import org.js.swiftcodes.api.model.Error;
 import org.js.swiftcodes.api.validation.BadRequestException;
 import org.js.swiftcodes.service.DeleteSwiftCodeService;
 import org.js.swiftcodes.service.SingleSwiftCodeGetService;
+import org.js.swiftcodes.service.exceptions.SwiftCodeInvalidException;
 import org.js.swiftcodes.service.exceptions.SwiftCodeNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,10 +17,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/v1/")
@@ -50,13 +54,17 @@ public class SwiftCodesController {
     }
 
     @DeleteMapping("swift-codes/{swift-code}")
-    public ResponseEntity<HttpStatus> deleteSwiftCode(@PathVariable("swift-code") String swiftCode) {
+    public ResponseEntity<Map<String, String>> deleteSwiftCode(@PathVariable("swift-code") String swiftCode, @RequestParam String bankName, @RequestParam String countryISO2Code) {
         if (swiftCode.length() != SWIFT_CODE_LENGTH) {
             throw new BadRequestException(String.format("Parameter swiftCode %s is invalid. SWIFT Code can't be empty and must have %d characters.", swiftCode, SWIFT_CODE_LENGTH));
         }
 
-        deleteSwiftCodeService.deleteSwiftCode(swiftCode);
-        return ResponseEntity.ok(HttpStatus.NO_CONTENT);
+        int deletedCount = deleteSwiftCodeService.deleteSwiftCode(swiftCode, bankName, countryISO2Code);
+
+        Map<String, String> response = new HashMap<>();
+        response.put("message", String.format("Successfully deleted %d record with SWIFT code: %s", deletedCount, swiftCode));
+
+        return ResponseEntity.ok(response);
     }
 
     @ExceptionHandler(SwiftCodeNotFoundException.class)
@@ -64,6 +72,15 @@ public class SwiftCodesController {
         Error response = Error.of("NOT_FOUND", ex.getMessage());
         log.warn("Handled SwiftCodeNotFoundException: " + ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(response);
+    }
+
+    @ExceptionHandler(SwiftCodeInvalidException.class)
+    public ResponseEntity<Error> handleInvalidSwiftCodeException(Exception ex, WebRequest request) {
+        Error response = Error.of("INVALID_INPUT", ex.getMessage());
+        log.warn("Handled SwiftCodeInvalidException: " + ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
             .contentType(MediaType.APPLICATION_JSON)
             .body(response);
     }
--- src/main/java/org/js/swiftcodes/service/DeleteSwiftCodeService.java
+++ src/main/java/org/js/swiftcodes/service/DeleteSwiftCodeService.java
@@ -1,9 +1,14 @@
 package org.js.swiftcodes.service;
 
+import org.js.swiftcodes.api.validation.BadRequestException;
+import org.js.swiftcodes.service.dao.entity.BankDataEntity;
 import org.js.swiftcodes.service.dao.mapper.BankDataMapper;
 import org.js.swiftcodes.service.exceptions.SwiftCodeNotFoundException;
+import org.js.swiftcodes.service.util.SwiftCodeUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 public class DeleteSwiftCodeService {
@@ -14,12 +19,31 @@ public class DeleteSwiftCodeService {
         this.bankDataMapper = bankDataMapper;
     }
 
-    public int deleteSwiftCode(String swiftCode) {
+    public int deleteSwiftCode(String swiftCode, String bankName, String countryISO2) {
+        validateInput(swiftCode, bankName, countryISO2);
+
         int deletedSwiftCode = bankDataMapper.deleteOne(swiftCode);
         if (deletedSwiftCode == 0) {
-            throw new SwiftCodeNotFoundException(String.format("Swift code %s not found.", swiftCode));
+            throw new SwiftCodeNotFoundException("Failed delete. Probably bank data was modified in other session. Please repeat operation.");
         }
 
         return deletedSwiftCode;
+    }
+
+    private void validateInput(String swiftCode, String bankName, String countryISO2) {
+        BankDataEntity fetchedBankData = bankDataMapper.selectOne(swiftCode);
+
+        SwiftCodeUtil.validateSwiftCode(swiftCode);
+        if (Objects.isNull(bankName)) {
+            throw new BadRequestException("Provided bank name mustn't be empty.");
+        } else if (Objects.isNull(countryISO2)) {
+            throw new BadRequestException("Provided country ISO2 mustn't be empty.");
+        } else if (fetchedBankData == null) {
+            throw new SwiftCodeNotFoundException(String.format("Swift code %s not found in database.", swiftCode));
+        } else if (!bankName.equals(fetchedBankData.getName()) || !countryISO2.equals(fetchedBankData.getCountryIso2Code())) {
+            throw new BadRequestException(String.format("Parameter %s is not a valid bank name and parameter %s is not a valid country ISO2.",
+                fetchedBankData.getName(),
+                fetchedBankData.getCountryIso2Code()));
+        }
     }
 }
